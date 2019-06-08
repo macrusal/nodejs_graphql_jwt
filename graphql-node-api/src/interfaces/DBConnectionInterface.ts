7809337cce5123429715953325b3ffe658051142
macrusal <macrusal@gmail.com>
2019-06-08 18:34:37 -0300

    Criando objetos de Banco de Dados - DBConnectionInterface

---
+++ graphql-node-api/src/interfaces/DBConnectionInterface.ts
@@ -0,0 +1,8 @@
+import * as Sequelize from 'sequelize';
+import { ModelsInterface } from './ModelsInterface';
+
+export interface DBConnection extends ModelsInterface {
+
+    sequelize: Sequelize.Sequelize;
+    
+}
