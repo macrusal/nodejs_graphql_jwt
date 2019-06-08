3cf501f0905ee3563ec3fdc87d8302feaf5a4364
macrusal <macrusal@gmail.com>
2019-06-07 22:51:23 -0300

    Configurando tsc - copiando app.ts - criandodiretorio dist com app.js

---
+++ graphql-node-api/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var ts = require('gulp-typescript');
+
+var tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('scripts', ['static'], () => {
+    var tsResult = tsProject.src()
+        .pipe(tsProject());
+
+    return tsResult.js
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('static', ['clean'], () => {
+    return gulp
+        .src(['src/**/*.json'])
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', () => {
+    return gulp
+        .src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build',['clean', 'static', 'scripts']);
